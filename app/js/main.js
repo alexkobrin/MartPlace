e1fa278dd2c457d4a4a33984f1f0d18a471b430e
alexkobrin <storyday2012@gmail.com>
2020-07-24 16:49:15 +0200

    added product page

--- app/js/main.js
+++ app/js/main.js
@@ -49,6 +49,20 @@ $ (function() {
         $('.menu__btn ').on('click' , function(){
           $('.menu__list').slideToggle()
         });
+        $('.categories-title').on('click' , function(){
+          $('.categorie-list__inner').toggleClass('active')
+        });
+        $('.filter-title').on('click' , function(){
+          $('.filter-list').toggleClass('active')
+        });
+
+        $(".js-range-slider").ionRangeSlider({
+          prefix: "$"
+        });
+
+
+
+
         $('.lnr-users').on('click' , function(){
           $('.header__box').toggleClass('active')
           $('.header__logo').toggleClass('active')
@@ -65,11 +79,22 @@ $ (function() {
           arrows:true,        
         });
 
-
+        $('.lnr-dice').on('click', function(){
+          $('.product__item-inner-box').removeClass('product__item-list');
+          $('.lnr-dice').addClass('active');
+          $('.lnr-list').removeClass('active');
+          
+        });
         
-        var mixer = mixitup('.product__item-inner');
+         $('.lnr-list').on('click', function(){
+          $('.product__item-inner-box').addClass('product__item-list');
+          $('.lnr-list').addClass('active');
+          $('.lnr-dice').removeClass('active');
+          
+        });
 
-  
+        var mixer = mixitup('.product__item-inner');
+      
     });
     
     
